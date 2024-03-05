fbc85c560403fbbc4887ac3836bdaf04f89b128a
raj <rajguptark99@gmail.com>
2024-03-05 17:15:24 +0530

    updated code in master branch

--- src/test/java/javaPractise/checkstaticMethod.java
+++ src/test/java/javaPractise/checkstaticMethod.java
@@ -7,13 +7,15 @@ public class checkstaticMethod {
 	
 
 	}
-	
+	//updating code in master branch
 	public void number(int a) {
-		
+		System.out.print("Updating in master branch");
+		System.out.print("Updating23 in master branch");
+		System.out.print("Updating33 in master branch");
 	}
 	
 	public static void number2(int b) {
-		
+		System.out.print("Updating2 in master branch");
 	}
 
 }
