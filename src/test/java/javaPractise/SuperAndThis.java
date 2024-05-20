675da609f948bd2f25fb7110a3696fa20b36aa37
raj <rajguptark99@gmail.com>
2024-05-20 14:13:47 +0530

    indian guy

--- src/test/java/javaPractise/SuperAndThis.java
+++ src/test/java/javaPractise/SuperAndThis.java
@@ -2,9 +2,29 @@ package javaPractise;
 
 class n{
 	
+	public n() {
+		System.out.println("default constructor");
+	}
+	
+	public n(int n) {
+		System.out.println("parametrized constructor");
+	}
+	
 	}
 
 class b extends n{
+	
+	public b() {
+		
+		System.out.println("default child constructor");
+	}
+	
+	public b(int a) {
+		super(a);
+		
+		
+		System.out.println("parametrized child constructor");
+	}
 	
 }
 
@@ -15,7 +35,7 @@ public class SuperAndThis {
 	
 	
 	public static void main(String[] args) {
-		b oj = new b(6, 7);
+		b oj = new b(6);
 
 	}
 
